418e573c95daa9a2231b3ee14a87d21ebe921bbe
Jennifer Huguenin <huguenin.jennifer@gmail.com>
2025-10-25 16:18:32 +0200

    completed messages controllers create method so that the user can keep chatting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,14 +2,23 @@ class MessagesController < ApplicationController
   before_action :set_chat
 
   def create
-    # Créer le message de l'utilisateur
+    # Crée le message de l'utilisateur
     @message = @chat.messages.build(message_params)
     @message.role = "user"
 
     if @message.save
-      # TODO: Appeler le LLM et créer la réponse
-      # Pour l'instant on redirige juste vers le chat
+      # Appelle le LLM et crée la réponse
+      @ruby_llm_chat = RubyLLM.chat
+      response = @ruby_llm_chat.with_instructions(SYSTEM_PROMPT).ask(@message.content)
+
+      #Crée la réponse
+      @chat.messages.create!(
+        role: "assistant",
+        content: response.content
+      )
+
       redirect_to chat_path(@chat)
+
     else
       # En cas d'erreur on redirige avec un message
       redirect_to chat_path(@chat), alert: "Error sending message"
