e102d85e43a86e34045a1a0dc062efb3960ab227
Nawel Paschy <naweljpaschy@gmail.com>
2025-10-25 21:48:07 +0200

    Edited form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_25_131751) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_25_192208) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_25_131751) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "mood"
+    t.string "genre"
+    t.string "actor"
+    t.string "realisator"
+    t.integer "year"
+    t.string "country"
+    t.integer "length"
     t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
