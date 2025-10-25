9fda1034251db13aa6c32eb5d34e6fd592d4d679
Jennifer Huguenin <huguenin.jennifer@gmail.com>
2025-10-25 14:03:24 +0200

    created message model and added dependencies to chat and messagemodels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_23_191002) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_25_115935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_23_191002) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "role"
+    t.text "content"
+    t.bigint "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -59,6 +68,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_23_191002) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "chats"
   add_foreign_key "recommendations", "chats"
   add_foreign_key "recommendations", "movies"
 end
