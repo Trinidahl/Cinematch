1c9ecf0606bf85ebeadfb8dee03af6f3fa89aa6d
vincent <vincent.horo@outlook.fr>
2025-10-26 13:42:36 +0100

    cloudinary set up

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,17 +6,26 @@ class Movie < ApplicationRecord
   has_many :recommendations
   has_many :chats, through: :recommendations
 
+  has_one_attached :poster
+
   after_create :attach_poster
 
   private
 
   def attach_poster
-    url = self.url
-    html = URI.open(url)
+    html = URI.open(self.url)
     html_doc = Nokogiri::HTML.parse(html)
+
     # recherche le code css correspondant, le premier et prend l'image
     poster = html_doc.at_css(".ipc-media--poster-l img")
     poster_src = poster['src'] # <-- on récupère l'URL de l'image qui est dans img
-    puts poster_src
+
+    file = URI.open(poster_src)
+    poster.attach(
+      io: file,
+      filename: "#{title.parameterize}.jpg",
+      content_type: "image/jpeg"
+    )
+    Rails.logger.info("Poster attaché pour #{title} via Cloudinary")
   end
 end
